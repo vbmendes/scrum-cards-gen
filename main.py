e2bb0d46ae60d3aa63480114abfcf2522b1a48e9
Vinicius Mendes <vbmendes@gmail.com>
2016-05-03 00:08:55 -0300

    Adiciona lorem ipsum para descrição, melhora a interface por linha de comando e ordena corretamente os cartões.

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
+#!/usr/bin/env python
 # coding: utf8
+
+from optparse import OptionParser
+import os
+
 import csv
 import math
+
+from loremipsum import generate_paragraph
+
 from reportlab.lib.enums import TA_LEFT, TA_CENTER
 from reportlab.lib.pagesizes import A4
 from reportlab.lib.styles import getSampleStyleSheet, ParagraphStyle
@@ -43,8 +51,16 @@ pdfmetrics.registerFont(TTFont('Esphimere Bold', 'fonts/Esphimere Bold.otf'))
 pdfmetrics.registerFont(TTFont('Esphimere', 'fonts/Esphimere.otf'))
 pdfmetrics.registerFont(TTFont('Arial', 'fonts/arial.ttf'))
 
+def truncate_string(string, size):
+    if len(string) > size:
+        i = size
+        while(string[i] not in (' ', '\n')):
+            i -= 1
+        string = string[:i] + '...'
+    return string
 
-def drawPdf(filename, cards):
+
+def draw_pdf(filename, cards):
     c = canvas.Canvas(filename, pagesize=A4)
 
     width, height = A4
@@ -61,24 +77,21 @@ def drawPdf(filename, cards):
     row = 0
     col = 0
     for card in cards:
-        print card['id']
         if cards_count % cards_per_page == 0:
-            print 'quebrou pagina'
             col = 0
-            row = 0
+            row = ROWS - 1
             if cards_count != 0:
                 page += 1
                 c.showPage()
             c.translate(MARGIN_LEFT, MARGIN_BOTTOM)
-            print 'inicio pagina' + str(page)
-        elif row == ROWS:
-            row = 0
-            col += 1
+        elif col == COLS:
+            col = 0
+            row -= 1
+        #elif row == -1:
+        #    row = ROWS - 1
+        #    col += 1
         print col, row
 
-        #card_i = cards_per_page*page + col*ROWS + row
-        #if card_i < len(cards):
-        #card = cards[card_i]
         origin = (col*(rect_width + MARGIN_BETWEEN_X), row*(rect_height + MARGIN_BETWEEN_Y))
         c.rect(origin[0], origin[1], rect_width, rect_height)
         padding = cm/4
@@ -95,14 +108,17 @@ def drawPdf(filename, cards):
 
         height_descricao = style_descricao.fontSize
         # TODO! Improve String split
-        descricao = card['descricao'][:600] + '...'
+        descricao = truncate_string(card['descricao'], 600)
         p = Paragraph(descricao, style=style_descricao)
         p.wrapOn(c, rect_width - padding*2, height_titulo * 6.5)
         p.drawOn(c, origin[0] + padding, origin[1] + rect_height - padding * 4.5 - height_id - height_titulo - height_titulo * 6.5)
 
-        row += 1
+        #row -= 1
+        col += 1
         cards_count += 1
     c.save()
+
+    print 'Created %s with %s cards in %s pages.' % (filename, cards_count, page+1)
 
 
 class CSVReader(object):
@@ -115,25 +131,28 @@ class CSVReader(object):
         return self
 
     def next(self):
+        _, _, paragraph = generate_paragraph()
         while True:
             row = self.csvreader.next()
             if row[0]:
                 return {
                     'id': row[0],
                     'titulo': row[1],
-                    'descricao': " ".join([titulo,]*10),
+                    'descricao': paragraph,
                     'estimativa': row[2],
                 }
 
-if __name__ == "__main__":
-    titulo = "Aqui vai o título da história no ALM que pode até quebrar a linha, mas dá tudo certo mesmo tendo que aumentar um pouquinho mais o texto"
-    cards = []
-    for i in range(27):
-        cards.append({
-            'id': 999999-i,
-            'titulo': "Aqui vai o título da história no ALM que pode até quebrar a linha, mas dá tudo certo mesmo tendo que aumentar um pouquinho mais o texto",
-            'descricao': " ".join([titulo,]*10)[:600],
-            'estimativa': "PM"
-        })
-    with open('itens.csv') as csvfile:
-        drawPdf('cards.pdf', CSVReader(csvfile))
+def main():
+    parser = OptionParser(usage=("usage: %prog [options] source_csv output_pdf"))
+
+    (options, args) = parser.parse_args()
+
+    source_csv = args[0]
+    output_pdf = args[1]
+    
+    with open(source_csv) as csvfile:
+        draw_pdf('cards.pdf', CSVReader(csvfile))
+
+
+if __name__ == '__main__':
+    main()
